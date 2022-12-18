f3f26d24880e730ce1a995a68219dff9aa92aa04
yoon-junseo <ysj6844@naver.com>
2022-12-18 16:20:44 +0900

    feat: button 추가

---
+++ src/Button/index.tsx
@@ -0,0 +1,12 @@
+import React, { Children } from 'react'
+
+interface Props {
+  onClick: () => void
+  children: React.ReactNode
+}
+
+const Button = ({ onClick, children }: Props) => {
+  return <button onClick={onClick}>{children}</button>
+}
+
+export default Button
